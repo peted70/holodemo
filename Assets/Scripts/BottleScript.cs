14d28b9ccc645b19b7f4013386e430421fc509d7
Pete Daukintis <peted70@hotmail.com>
2016-09-25 19:22:03 +0100

    initial add

---
+++ Assets/Scripts/BottleScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using HoloToolkit.Unity;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BottleScript : MonoBehaviour
+{
+    public void OnSelect()
+    {
+        // HitInfo
+        var rayHit = GazeManager.Instance.HitInfo;
+        GetComponent<Rigidbody>().AddForceAtPosition(rayHit.normal * -5, rayHit.point, ForceMode.Impulse);
+
+        Debug.Log("Hit the bottle");
+    }
+}
---
+++ Assets/VisrTestScene/ExplodingBottle.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ExplodingBottle : MonoBehaviour {
+
+    void OnLookStateAction(RaycastHit rayHit)
+    {
+        
+        GetComponent<Rigidbody>().AddForceAtPosition(rayHit.normal * -5, rayHit.point, ForceMode.Impulse);
+    }
+}
